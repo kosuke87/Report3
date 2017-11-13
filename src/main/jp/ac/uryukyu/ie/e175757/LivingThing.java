d4c88d0b54a6b011ac66d9633d5e43ce5b5c1832
Kosuke Hanashiro <e175757@ie.u-ryukyu.ac.jp>
2017-11-13 20:38:37 +0900

    report_data

--- src/main/jp/ac/uryukyu/ie/e175757/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e175757/LivingThing.java
@@ -5,4 +5,35 @@ public class LivingThing {
     int hitPoint;
     int attack;
     boolean dead;
+
+    public LivingThing(String name,int maximumHP,int attack){
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+    public boolean isDead(){
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if(dead!=true) {
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
 }
